deb52908df5e7064e683cc1c3d9d8401c12cafa6
Lookuz Ubuntu <cweyyeh512@gmail.com>
2020-05-25 01:08:02 +0800

    Added non-stationary kernels

--- kernels.py
+++ kernels.py
@@ -32,6 +32,15 @@ def poly(X1, X2, bias=1., order=2.):
 
     return result
 
+# Rational Quadratic kernel
+# Generalisation over the squared exponential kernel
+def rational_quadratic(X1, X2, alpha=1.0, l=1.0):
+
+    sq_norm = cdist(X1, X2, metric='sqeuclidean')
+
+    K = (1 + (sq_norm / (2. * alpha * (l**2))))**alpha
+    return K
+
 # Matern Kernel 
 # Special cases for nv = 0.5, 1.5, 2.5
 # For other values that not as above, the general Matern kernel formula will be used
@@ -40,19 +49,42 @@ def matern(X1, X2, nv=1.5, l=1.0):
     X1 = X1 / l
     X2 = X2 / l
 
-    abs_dist = cdist(X1, X2, metric='euclidean')
+    norm = cdist(X1, X2, metric='euclidean')
 
     if nv == 0.5:
-        K = np.exp(-abs_dist)
+        K = np.exp(-norm)
     elif nv == 1.5:
-        r = math.sqrt(3.0) * abs_dist
+        r = math.sqrt(3.0) * norm
         K = (1.0 + r) * np.exp(-r)
     elif nv == 2.5:
-        r = math.sqrt(5) * abs_dist
+        r = math.sqrt(5) * norm
         K = (1.0 + r + (r**2 / 3.0)) * np.exp(-r)
     else:
         a = (2.0**(1.0 - nv)) / gamma(nv)
-        R =  math.sqrt(2.0 * nv) * abs_dist
+        R =  math.sqrt(2.0 * nv) * norm
         K = a * (R**nv) * kv(nv, R)
+
+    return K
+
+# Exponential sine squared kernel
+# Defined by k(x, x') = exp(-(2 sin^2 (x - x'/2)) / l^2)
+def exp_sin_sq(X1, X2, l=1.0):
+    
+    norm = cdist(X1, X2, metric='euclidean')
+
+    K = np.exp(-2.0 * (np.sin(norm/2.0) / l)**2)
+    return K
+
+# Neural network covariance kernel
+# sigma indicates the covariance matrix over u, the neural network
+# weight parameters
+def nn(X1, X2, alpha=1.0, omega=1.0, b=1.0, sigma=None):
+    if sigma == None:
+        sigma = np.eye(len(X1[0]))
+
+    X1X2 = 2 * X1.T.dot(sigma).dot(X2)
+    X1_2 = 2.*X1.T.dot(sigma).dot(X1) + 1.
+    X2_2 = 2.*X2.T.dot(sigma).dot(X2) + 1. 
+    K = np.arcsin(X1X2 / np.sqrt(X1_2 * X2_2))
 
     return K
